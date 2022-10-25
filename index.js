e2abe02784555a2c2ccd67c317fbd483b10cffc4
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-10-25 20:19:14 +0600

    get-course-categories

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courseCategories = require('./data/course-categories.json');
 
 app.get('/', (req, res) => {
     res.send('working on this project ');
 });
 
+app.get('/course-categories', (req, res) => {
+    res.send(courseCategories)
+});
 
 app.listen(port, () => {
     console.log('API Running on Port', port);
